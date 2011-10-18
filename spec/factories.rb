5ec52023b5c5390290c56c7e81edfd896c29e5dc
Marc Tyson <cervus0123@yahoo.co.uk>
2011-10-18 16:48:27 +0100

    added factories

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Marc Tyson"
+  user.email                  "marctyson84@yahoo.co.uk"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
